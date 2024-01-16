3fbf3e459d49e720d1cabdd62221f61098641aee
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-01-16 12:24:16 +0000

    wired Redux store to React side of the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+//! Notes
+// To wire up our Redux store to the React side of our application, 
+// 1- Import 'Provider' 
+// 2- Import 'store'
+// 3- Wrap 'provider' around '<app />'
+// 4- Then provide our store to the provider component as a prop called store.
+
 import "./index.css"
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+
+import { store } from "./store";
 import App from "./App";
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>   
 );
