760d7098c670ea5e82df224a16161d0a69ac5c82
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-01-18 15:56:32 +0000

    added Panel.js component

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import "./index.css"
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-
 import { store } from "./store";
 import App from "./App";
 
@@ -21,3 +20,13 @@ root.render(
         <App />
     </Provider>   
 );
+
+// We want to show that the user with a particular name has some albums that belong to them. Example: 
+// userA has 2 albums, album1 and album2. or userB has 3 albums, album3, album4, album5.
+// The same thing applies for our photos. So photos conceptually belong to one single album. Example: 
+// album1 has 3 photos, photo1, photo2, photo3. So inside of our code, inside of both our Redux store and  JSON server, We 
+// need to somehow make these relationships clear.
+
+// So in no scenario ever are we ever gonna try to make a request from inside of a reducer function. Reducers should always 
+// be a hundred percent synchronous. So they should always take in some state and action, make an update to state, and that's it.
+// They're never gonna have any API calls, no promises, no Async await, nothing like that.
