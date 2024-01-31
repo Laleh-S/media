395d378318cba2c9ad5aa9cccc6d46f766f8f5ac
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-01-31 19:42:56 +0000

    added expanded state and icons to header inside ExpandablePanel component

--- src/components/ExpandablePanel.js
+++ src/components/ExpandablePanel.js
@@ -1,16 +1,27 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function ExpandablePanel ({ header, children }) {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleClick = () => {
+        setExpanded(!expanded);
+    };
 
     return (
         <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
+            <div className="flex p-2 justify-between items-center">
                 <div className="flex flex-row items-center justify-between">
                     {header}
                 </div>
+                <div onClick={handleClick} className="cursor-pointer">
+                    {/* If expanded is true, then show the GoChevronDown icon. Else, GoChevronLeft icon */}
+                    {expanded ? <GoChevronDown /> : <GoChevronLeft />}
+                </div>
+                
             </div>
-            <div className="p-2 border-t">
-                {children}
-            </div>
+                {/* if expanded is true, we show the children div and everything inside of it */}
+                {expanded && <div className="p-2 border-t">{children}</div>}   
         </div>
     );
 };
