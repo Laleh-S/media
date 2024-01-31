1861065b46fffd421eecbbfc14bfc8116bea5b0e
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-01-31 19:12:51 +0000

    created ExpandablePanel component added header section inside UsersListItem

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,7 @@ import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
 
 function UsersListItem ({ user }) {
     // Wrapping up removeUser thunk with useThunk hook
@@ -11,22 +12,22 @@ function UsersListItem ({ user }) {
         doRemoveUser(user); // Passing in the user we want to delete
     };
 
+    const header = <>
+        {/* To indicate if the button should show the loading spinner, we pass in the "loading" prop. */}
+        {/* Whenever user clicks this button we want to run our removeUser thunk */}
+        <Button className= "mr-3 " loading={isLoadig} onClick={handleClick}>
+            <GoTrashcan /> 
+        </Button>
+        {/* If there is an error, print out a div that says: error deleting user */}
+        {error && <div>Error Deleting User...</div>}
+        {user.name}
+    </>;
+
     return (
-        <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                <div className="flex flex-row items-center justify-between">
-                    {/* To indicate if the button should show the loading spinner, we pass in the "loading" prop. */}
-                    {/* Whenever user clicks this button we want to run our removeUser thunk */}
-                    <Button className= "mr-3 " loading={isLoadig} onClick={handleClick}>
-                        <GoTrashcan /> 
-                    </Button>
-                    {/* If there is an error, print out a div that says: error deleting user */}
-                    {error && <div>Error Deleting User...</div>}
-                    {user.name}
-                </div>
-            </div>
-        </div>
-    );
+        <ExpandablePanel header={header}>
+            CONTENT!!!!
+        </ExpandablePanel>
+    );      
 };
 
 export default UsersListItem;
