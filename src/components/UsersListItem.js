f421a43d3f76d20202c28672e12b6ef4b11e8cd0
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-01-31 15:20:46 +0000

    deleting user successfully

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,9 +1,26 @@
+import { GoTrashcan } from "react-icons/go";
+import Button from "./Button";
+import { removeUser } from "../store";
+import { useThunk } from "../hooks/use-thunk";
 
 function UsersListItem ({ user }) {
+    // Wrapping up removeUser thunk with useThunk hook
+    const [doRemoveUser, isLoadig, error] = useThunk(removeUser)
+
+    const handleClick = () => {
+        doRemoveUser(user); // Passing in the user we want to delete
+    };
 
     return (
         <div className="mb-2 border rounded">
             <div className="flex p-2 justify-between items-center cursor-pointer">
+                {/* To indicate if the button should show the loading spinner, we pass in the "loading" prop. */}
+                {/* Whenever user clicks this button we want to run our removeUser thunk */}
+                <Button loading={isLoadig} onClick={handleClick}>
+                    <GoTrashcan /> 
+                </Button>
+                {/* If there is an error, print out a div that says: error deleting user */}
+                {error && <div>Error Deleting User...</div>}
                 {user.name}
             </div>
         </div>
@@ -11,3 +28,4 @@ function UsersListItem ({ user }) {
 };
 
 export default UsersListItem;
+
