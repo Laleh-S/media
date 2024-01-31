89280dca1c7a651f81fb3215554dc4f95d41136e
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-01-31 15:28:08 +0000

    added some styling to UsersListeItem component

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -14,14 +14,16 @@ function UsersListItem ({ user }) {
     return (
         <div className="mb-2 border rounded">
             <div className="flex p-2 justify-between items-center cursor-pointer">
-                {/* To indicate if the button should show the loading spinner, we pass in the "loading" prop. */}
-                {/* Whenever user clicks this button we want to run our removeUser thunk */}
-                <Button loading={isLoadig} onClick={handleClick}>
-                    <GoTrashcan /> 
-                </Button>
-                {/* If there is an error, print out a div that says: error deleting user */}
-                {error && <div>Error Deleting User...</div>}
-                {user.name}
+                <div className="flex flex-row items-center justify-between">
+                    {/* To indicate if the button should show the loading spinner, we pass in the "loading" prop. */}
+                    {/* Whenever user clicks this button we want to run our removeUser thunk */}
+                    <Button className= "mr-3 " loading={isLoadig} onClick={handleClick}>
+                        <GoTrashcan /> 
+                    </Button>
+                    {/* If there is an error, print out a div that says: error deleting user */}
+                    {error && <div>Error Deleting User...</div>}
+                    {user.name}
+                </div>
             </div>
         </div>
     );
