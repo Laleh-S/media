ac11e4b0f334b817a263c5ca857ae8507cbaff8e
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-02-01 21:14:51 +0000

    showing list of albums, everything is working well

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,4 +1,7 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 function AlbumsList ({ user }) {
     // Whenever we call this hook, we are going to immediately try to fetch some data.
@@ -9,8 +12,29 @@ function AlbumsList ({ user }) {
     // isLoading is gonna be a Boolean true or false.  It's gonna be true if we are currently in the process of making the request.
     
     const { isLoading, data, error } = useFetchAlbumsQuery(user);   //* step 9 of Creating a RTK Query API
-    console.log(data, error, isLoading)
-    return <div>Albums for {user.name}</div>
+
+    let content;
+    //  If isLoading is true, then set content to be skeleton.
+    if (isLoading){
+        content = <Skeleton times={3} /> // 3 is the number of boxex
+    } else if (error){ // Else if there is an error object, we set content to be a div with an error message.
+        content = <div>Error loading albums.</div>
+    } else {
+        // Else, map over the array of data object and for each one, create an ExpandablePanel with the header of the album's title.
+        content = data.map((album) => {
+            const header = <div>{album.title} </div>;
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos in the album
+            </ExpandablePanel>
+        });
+    }
+
+    return (
+        <div>
+            <div>Albums for {user.name}</div>
+            <div>{content}</div>
+        </div>
+    )
 };
 
 export default AlbumsList;
